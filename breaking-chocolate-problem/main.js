20e7e388d24848a5b0328e39304cfb83ac3db17e
tothricsaj <tothricsaj@gmail.com>
2019-12-15 10:50:16 +0100

    test cases are great :smirk:

--- breaking-chocolate-problem/main.js
+++ breaking-chocolate-problem/main.js
@@ -1,7 +1,13 @@
 function breakChocolate(n,m) {
-   let countOfBreak = (n * m) - 1;
-    return countOfBreak;
+    if (n <= 0 || m <= 0) return 0;
+    else return (n * m) - 1;
 }
 
 console.log(breakChocolate(5,5));
+console.log(breakChocolate(1,1));
+console.log(breakChocolate(2,1));
+console.log(breakChocolate(3,1));
+console.log(breakChocolate(0,1));
+
+
 
