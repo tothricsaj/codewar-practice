a85a9bb8b5f34238a58c4f7b7961e9a8f4b229e6
tothricsaj <tothricsaj@gmail.com>
2019-12-16 12:57:41 +0100

    almost brainfck :boat:

--- schrödinger-s-boolean/main.js
+++ schrödinger-s-boolean/main.js
@@ -2,4 +2,5 @@ const omnibool = {n:true, valueOf: ()=>this.n=!this.n};
 
 console.log(omnibool == true);
 console.log(omnibool == false);
-console.log(omnibool);
+console.log(+omnibool);
+console.log(!+omnibool);
