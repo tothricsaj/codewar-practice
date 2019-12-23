3d245ec37f0cf8216888596242d065259d0a18c6
tothricsaj <tothricsaj@gmail.com>
2019-12-23 12:57:20 +0100

    counted mouses correctly :bus:

--- the-deaf-rats-of-hamelin.js
+++ the-deaf-rats-of-hamelin.js
@@ -1,17 +1,23 @@
 var countDeafRats = function(town) {
     let PPposition;
-    let leftMouse, rightMouse = 0;
+    let leftMouse = 0, 
+        rightMouse = 0;
     
     for(let i=0; i<town.length; i++) {
         if(town[i] == '~' && town[i+1] == 'O') {
             rightMouse++;
+            i++;
         } else if(town[i] == 'O' && town[i+1] == '~') {
             leftMouse++;
+            i++;
+        } else {
+            continue;
         }
     }
 
     console.log(`rightMouse -> ${rightMouse}`);
     console.log(`leftMouse -> ${leftMouse}`);
+    console.log('<---------------------------->');
 
     return 0;
 }
