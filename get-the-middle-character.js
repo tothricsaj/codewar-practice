a8e3f08f1a54d3efbdd6fef9fc240b7209651858
tothricsaj <tothricsaj@gmail.com>
2019-12-24 11:29:50 +0100

    more elegant solution :bus:

--- get-the-middle-character.js
+++ get-the-middle-character.js
@@ -1,3 +1,4 @@
+/*
 function getMiddle(s) {
     if (s.length % 2 == 0) return s.substring(
         Math.floor((s.length - 1) / 2), 
@@ -5,7 +6,13 @@ function getMiddle(s) {
     );
     else return s[Math.floor((s.length - 1) / 2)];
 }
+*/
+
+function getMiddle(s) {
+    console.log(s.length / 2 - 1);
+    return s.substring(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2:1);
+}
 
 console.log(getMiddle("test"));
-console.log(getMiddle('A'));
-console.log(getMiddle('testing'));
+// console.log(getMiddle('A'));
+// console.log(getMiddle('testing'));
