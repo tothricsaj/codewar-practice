3d42b7bcc359c5c1ce0adb5accd664503264c9a8
tothricsaj <tothricsaj@gmail.com>
2019-12-24 10:14:48 +0100

    shortest word

---
+++ shortest-word.js
@@ -0,0 +1,7 @@
+function findShort(s){
+    let strArr = s.split(' ');
+    return strArr;
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+// findShort("turns out random test cases are easier than writing out basic ones");
