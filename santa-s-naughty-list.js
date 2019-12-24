70766c6e511b3370485aefe05f79752d99b3e22e
tothricsaj <tothricsaj@gmail.com>
2019-12-24 10:48:10 +0100

    sorting array

--- santa-s-naughty-list.js
+++ santa-s-naughty-list.js
@@ -1,6 +1,7 @@
 function findChildren(santasList, children) {
-    return children.filter(child => santasList.indexOf(child) != -1);
+    return children.filter(child => santasList.indexOf(child) != -1).sort();
 }
 
 console.log(findChildren(["Jason", "Jackson", "Jordan", "Johnny"], ["Jason", "Jordan", "Jennifer"]));
 console.log(findChildren(["jASon", "JAsoN", "JaSON", "jasON"], ["JasoN", "jASOn", "JAsoN", "jASon", "JASON"]));
+console.log(findChildren(["Jason", "James", "Johnson"], ["Jason", "James", "JJ"]));
