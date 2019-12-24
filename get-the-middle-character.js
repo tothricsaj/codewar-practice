7c3cc69334de01c1ba8ed8522f03d252e2d7d641
tothricsaj <tothricsaj@gmail.com>
2019-12-24 11:22:29 +0100

    solved

--- get-the-middle-character.js
+++ get-the-middle-character.js
@@ -1,7 +1,11 @@
 function getMiddle(s) {
-
+    if (s.length % 2 == 0) return s.substring(
+        Math.floor((s.length - 1) / 2), 
+        Math.floor((s.length - 1) / 2) + 2
+    );
+    else return s[Math.floor((s.length - 1) / 2)];
 }
 
 console.log(getMiddle("test"));
-// console.log(getMiddle('A'));
-// console.log(getMiddle('testing'));
+console.log(getMiddle('A'));
+console.log(getMiddle('testing'));
