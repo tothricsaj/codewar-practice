36532f07302505ccb1f0a2367133d44e6cee739c
tothricsaj <tothricsaj@gmail.com>
2019-12-24 10:20:59 +0100

    working shortest word yheaa :smirk:

--- shortest-word.js
+++ shortest-word.js
@@ -1,7 +1,12 @@
 function findShort(s){
-    let strArr = s.split(' ');
-    return strArr;
+    let strArr = s.split(' '),
+        shortestLenght = 100000000000;
+
+    strArr.forEach(val => {
+        if(val.length < shortestLenght) shortestLenght = val.length
+    });
+    return shortestLenght;
 }
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
-// findShort("turns out random test cases are easier than writing out basic ones");
+console.log(findShort("turns out random test cases are easier than writing out basic ones"));
