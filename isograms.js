9623e997cace17b77eab630ff9a13e09434c4e04
tothricsaj <tothricsaj@gmail.com>
2019-12-28 11:27:38 +0100

    working???

--- isograms.js
+++ isograms.js
@@ -1,7 +1,14 @@
 function isIsogram(str){
-  //...
+    str = str.toLowerCase();
+    for(let i=0; i<str.length; i++) {
+        if(str.includes(str[i], i+1)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 console.log(isIsogram("Dermatoglyphics"));
 console.log(isIsogram("isogram"));
-console.log(isIsogram("isogram"));
+console.log(isIsogram("moOse"));
