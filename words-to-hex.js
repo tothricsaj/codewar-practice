ac588cf389f1c9d2f263b296a8a2277df86bbc95
tothricsaj <tothricsaj@gmail.com>
2019-12-28 13:54:52 +0100

    :poop:

--- words-to-hex.js
+++ words-to-hex.js
@@ -1,5 +1,19 @@
 function wordsToHex(words) {
-  
+    let arr = words.split(' ');
+
+    return arr.reduce((acc, curr) => {
+        let tmp = '#';
+
+        if(curr.length >= 3) {
+
+            console.log(curr);
+            tmp += curr[0].charCodeAt();
+            tmp += curr[1].charCodeAt();
+            tmp += curr[2].charCodeAt();
+        }
+
+        return tmp;
+    });
 }
 
 console.log(wordsToHex("Hello, my name is Gary and I like cheese."));
