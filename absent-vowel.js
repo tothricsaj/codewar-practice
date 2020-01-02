c7dfd3f32a46920f651ae69ae06726ed3c72afea
tothricsaj <tothricsaj@gmail.com>
2020-01-02 16:25:53 +0100

    one solution

--- absent-vowel.js
+++ absent-vowel.js
@@ -1,8 +1,9 @@
 String.prototype.filter = Array.prototype.filter;
 
 function absentVowel(x){
-    return x.filter(w => w === 'p');
+    let vowels = ['a','e','i','o','u'];
+    return vowels.indexOf(vowels.filter(el => !x.includes(el))[0]);
 }
 
-absentVowel("John Doe hs seven red pples under his bsket")
-absentVowel("Bb Smith sent us six neatly arranged range bicycles");
+console.log(absentVowel("John Doe hs seven red pples under his bsket"));
+console.log(absentVowel("Bb Smith sent us six neatly arranged range bicycles"));
