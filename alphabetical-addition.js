d4eeff469176592e4e39ce977bc5956ea2e0bad6
tothricsaj <tothricsaj@gmail.com>
2020-01-03 16:50:53 +0100

    :poop:

--- alphabetical-addition.js
+++ alphabetical-addition.js
@@ -3,15 +3,23 @@ function addLetters(...letters) {
 
     return String.fromCharCode(
         letters.map(el => el.charCodeAt() - 96)
-        .reduce((acc, el) => {
-            if (acc >= 26) acc = 0;
+        .reduce((acc, el, index, arr) => {
+            if (acc >= 26) {
+                console.log(`before acc -> ${acc} el -> ${el}`);
+                acc = (acc - 26) + el;
+                console.log(`after ${acc}`);
+            }
+
+            console.log(`before return acc -> ${acc}`);
             return acc + el
         }) + 96);
 }
 
+/*
 console.log(addLetters('a', 'b', 'c'))
 console.log(addLetters('a', 'b'))
-console.log(addLetters('z'))
-console.log(addLetters('z', 'a'))
+*/
+// console.log(addLetters('z'))
+// console.log(addLetters('z', 'a'))
 console.log(addLetters('y', 'c', 'b'))
-console.log(addLetters())
+// console.log(addLetters())
