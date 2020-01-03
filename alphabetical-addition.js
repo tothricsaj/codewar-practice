c42efbf6b593f9fce736f65737c73285219408f4
tothricsaj <tothricsaj@gmail.com>
2020-01-03 16:13:49 +0100

    alphabet addition is added :rocket:

---
+++ alphabetical-addition.js
@@ -0,0 +1,11 @@
+function addLetters(...letters) {
+  // your code here
+}
+
+console.log(addLetters('a', 'b', 'c'))
+console.log(addLetters('a', 'b'))
+console.log(addLetters('z'))
+console.log(addLetters('z', 'a'))
+console.log(addLetters('y', 'c', 'b'))
+console.log(addLetters())
+
