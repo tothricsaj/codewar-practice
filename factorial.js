c23dff914ff514e03169ffd082155d14b571e456
tothricsaj <tothricsaj@gmail.com>
2020-01-03 16:02:28 +0100

    it is working well :boat:

--- factorial.js
+++ factorial.js
@@ -1,9 +1,19 @@
 function factorial(n){
-  //your code here
+    let res = n;
+
+    if (n === 0) return 1;
+    else if (n === 1) return 1;
+
+    for(let i=n-1; i>0; i--) {
+        res *= i;
+    }
+
+    return res;
 }
 
 console.log(factorial(0));
 console.log(factorial(1));
-console.log(factorial(24));
+console.log(factorial(3));
+console.log(factorial(4));
 console.log(factorial(7));
 console.log(factorial(17));
