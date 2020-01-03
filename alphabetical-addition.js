c71b2659529ef6b83e1538f720ef7ac6251fa210
tothricsaj <tothricsaj@gmail.com>
2020-01-03 16:31:36 +0100

    overflow but not correct :poop:

--- alphabetical-addition.js
+++ alphabetical-addition.js
@@ -1,7 +1,10 @@
 function addLetters(...letters) {
+    if (letters.length === 0) return 'z';
+
     return String.fromCharCode(
         letters.map(el => el.charCodeAt() - 96)
         .reduce((acc, el) => {
+            if (acc >= 26) acc = 0;
             return acc + el
         }) + 96);
 }
@@ -11,4 +14,4 @@ console.log(addLetters('a', 'b'))
 console.log(addLetters('z'))
 console.log(addLetters('z', 'a'))
 console.log(addLetters('y', 'c', 'b'))
-// console.log(addLetters())
+console.log(addLetters())
