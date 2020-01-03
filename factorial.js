7ecb9ffbd56015adbd18549be5530cab66bd1cd5
tothricsaj <tothricsaj@gmail.com>
2020-01-03 15:54:38 +0100

    factorial is added :duck:

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function factorial(n){
+  //your code here
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(24));
+console.log(factorial(7));
+console.log(factorial(17));
