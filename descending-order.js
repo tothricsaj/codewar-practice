f566493a600319de64e1d64493e76ae5efc32b95
tothricsaj <tothricsaj@gmail.com>
2020-01-04 19:28:23 +0100

    good

--- descending-order.js
+++ descending-order.js
@@ -1,11 +1,8 @@
 function descendingOrder(n){
-    return new String(n).split('').map(el=>new Number(el).valueOf()).reverse()//.join('')
-  return new Number(new String(n).split('').reverse().join('')).valueOf();
+  return new Number(new String(n).split('').sort().reverse().join('')).valueOf();
 }
 
-/*
 console.log(descendingOrder(123456789))
 console.log(descendingOrder(1))
 console.log(descendingOrder(0))
-*/
 console.log(descendingOrder(13021))
