20f9936279117031af325a7f6c599bfbc39d7947
tothricsaj <tothricsaj@gmail.com>
2020-01-04 19:08:07 +0100

    refact:_more elegant solution

--- alphabetical-addition.js
+++ alphabetical-addition.js
@@ -1,27 +1,11 @@
 function addLetters(...letters) {
-    if (letters.length === 0) return 'z';
-
-    let numChar = letters.map(el => el.charCodeAt() - 96)
-           .reduce((acc, el, index, arr) => {
-                if (acc >= 26) {
-                acc = (acc - 26);
-            }
-
-            return acc + el
-    })
-
-    if(numChar > 26) numChar -= 26;
-
-    return String.fromCharCode(numChar + 96);
-
+    return String.fromCharCode((letters.reduce((a, b) => a+b.charCodeAt()-96, 0)+25)%26+97)
 }
 
-/*
 console.log(addLetters('a', 'b', 'c'))
 console.log(addLetters('a', 'b'))
-*/
-// console.log(addLetters('z'))
-// console.log(addLetters('z', 'a'))
-// console.log(addLetters('y', 'c', 'b'))
+console.log(addLetters('z'))
+console.log(addLetters('z', 'a'))
+console.log(addLetters('y', 'c', 'b'))
 console.log(addLetters("s", "a", "u", "z"))
 console.log(addLetters("y", "x", "i"))
