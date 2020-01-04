9953f2b3d1c1505e9881b5aaab719f586e8caba5
tothricsaj <tothricsaj@gmail.com>
2020-01-04 18:50:33 +0100

    refactoring :fear:

--- alphabetical-addition.js
+++ alphabetical-addition.js
@@ -1,18 +1,16 @@
 function addLetters(...letters) {
     if (letters.length === 0) return 'z';
 
-    return String.fromCharCode(
-        letters.map(el => el.charCodeAt() - 96)
-        .reduce((acc, el, index, arr) => {
-            if (acc >= 26) {
-                console.log(`before acc -> ${acc} el -> ${el}`);
-                acc = (acc - 26) + el;
-                console.log(`after ${acc}`);
+    let numChar = letters.map(el => el.charCodeAt() - 96)
+           .reduce((acc, el, index, arr) => {
+                if (acc >= 26) {
+                acc = (acc - 26);
             }
 
-            console.log(`before return acc -> ${acc}`);
             return acc + el
-        }) + 96);
+    })
+
+    return String.fromCharCode(numChar + 96);
 }
 
 /*
@@ -21,5 +19,6 @@ console.log(addLetters('a', 'b'))
 */
 // console.log(addLetters('z'))
 // console.log(addLetters('z', 'a'))
-console.log(addLetters('y', 'c', 'b'))
-// console.log(addLetters())
+// console.log(addLetters('y', 'c', 'b'))
+console.log(addLetters("s", "a", "u", "z"))
+console.log(addLetters("y", "x", "i"))
