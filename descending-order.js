39631addbf5aecd05209becf707d0f14bd621425
tothricsaj <tothricsaj@gmail.com>
2020-01-04 19:25:01 +0100

    wtf :poop:

--- descending-order.js
+++ descending-order.js
@@ -1,7 +1,11 @@
 function descendingOrder(n){
+    return new String(n).split('').map(el=>new Number(el).valueOf()).reverse()//.join('')
   return new Number(new String(n).split('').reverse().join('')).valueOf();
 }
 
+/*
 console.log(descendingOrder(123456789))
 console.log(descendingOrder(1))
 console.log(descendingOrder(0))
+*/
+console.log(descendingOrder(13021))
