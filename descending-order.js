78e5d1745688c9e13193df85bc09f3b4dc073d11
tothricsaj <tothricsaj@gmail.com>
2020-01-04 19:18:33 +0100

    solution

--- descending-order.js
+++ descending-order.js
@@ -1,5 +1,7 @@
 function descendingOrder(n){
-  
+  return new Number(new String(n).split('').reverse().join('')).valueOf();
 }
 
 console.log(descendingOrder(123456789))
+console.log(descendingOrder(1))
+console.log(descendingOrder(0))
