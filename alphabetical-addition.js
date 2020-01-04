cd777b2e6ee93547fbf118e10c4b84de6ea80af0
tothricsaj <tothricsaj@gmail.com>
2020-01-04 18:54:02 +0100

    refact:_simply code

--- alphabetical-addition.js
+++ alphabetical-addition.js
@@ -10,8 +10,9 @@ function addLetters(...letters) {
             return acc + el
     })
 
-    if(numChar <= 26) return String.fromCharCode(numChar + 96);
-    else return String.fromCharCode((numChar - 26) + 96);
+    if(numChar > 26) numChar -= 26;
+
+    return String.fromCharCode(numChar + 96);
 
 }
 
