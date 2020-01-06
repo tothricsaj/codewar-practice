d61c346b7c1c7463f7f722b5bc900b0a8a9843ae
tothricsaj <tothricsaj@gmail.com>
2020-01-06 16:01:28 +0100

    christmas tree is added

---
+++ 6kyu/christmas-tree.js
@@ -0,0 +1,11 @@
+function christmasTree(height) {
+
+}
+
+// console.log(christmasTree(1))
+// console.log(christmasTree(2))
+// console.log(christmasTree(3))
+// console.log(christmasTree(4))
+console.log(christmasTree(5))
+console.log(christmasTree(6))
+
