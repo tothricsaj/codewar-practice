87d826016009ca6517ad59d19e21d93c11878a50
tothricsaj <tothricsaj@gmail.com>
2020-01-07 16:35:48 +0100

    delete the last \n

--- 6kyu/christmas-tree.js
+++ 6kyu/christmas-tree.js
@@ -7,21 +7,18 @@ function christmasTree(height) {
         let pads = new Array(padsLength).fill(' ').join('');
         let stars = new Array(starLength).fill('*').join('');
 
-        tree += pads;
-        tree += stars;
-        tree += pads;
-        tree += '\n';
+        tree += pads + stars + pads + '\n';
 
         padsLength--;
         starLength += 2;
     }
 
-    return tree;
+    return tree.substr(0, tree.length - 1);
 }
 
-// console.log(christmasTree(1))
-// console.log(christmasTree(2))
-// console.log(christmasTree(3))
-// console.log(christmasTree(4))
+console.log(christmasTree(1))
+console.log(christmasTree(2))
+console.log(christmasTree(3))
+console.log(christmasTree(4))
 console.log(christmasTree(5))
-// console.log(christmasTree(6))
+console.log(christmasTree(6))
