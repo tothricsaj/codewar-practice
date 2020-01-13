3bf95d42dc73e34281898b8dd132cbe60129f7f3
tothricsaj <tothricsaj@gmail.com>
2020-01-13 16:02:08 +0100

    :duck: 8 and 9 is decoding

--- 6kyu/binaries.js
+++ 6kyu/binaries.js
@@ -1,3 +1,5 @@
+// String.prototype.map = Array.prototype.map;
+
 function code(strng) {
     let numbers = strng.split(''),
         binar = [];
@@ -25,10 +27,27 @@ function code(strng) {
 }
 
 function decode(str) {
-    // your code
+    let binArr = [];
+    for(let i = str.length; i>0; i--) {
+        if(str.substring(i, i-4) === '1000') {
+            binArr.push('8');
+            continue;
+        } else if(str.substring(i, i-4) === '1001') {
+            binArr.push('9');
+            continue;
+
+        }
+    }
+
+    return binArr;
 }
 
-console.log(code('3'));
-console.log(code('62'));
-console.log(code('213'));
+// console.log(code('8'));
+// console.log(code('9'));
+
+// console.log(code('62'));
+// console.log(code('213'));
+
+console.log(decode(code('8')));
+console.log(decode(code('9')));
 
