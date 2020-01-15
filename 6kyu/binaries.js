a202116286090158d77df116d926fc52f007b218
tothricsaj <tothricsaj@gmail.com>
2020-01-15 14:00:15 +0100

    stacked :poop:

--- 6kyu/binaries.js
+++ 6kyu/binaries.js
@@ -20,19 +20,37 @@ function code(strng) {
     });
 
     return binar.map(el => {
+
         if(el.length === 0) return '10'
+
         let digits = '0'.repeat(el.length -1);
         digits = digits + '1';
+
         return digits + el;
+        
     }).join('');
 }
 
 function decode(str) {
-    let binArr = [];
-    let counter = 0;
-    let decimal = 0;
+    let binArr = [],
+        counter = 0,
+        decimal = 0;
     
     for(let i=0; i<str.length; i++) {
+
+        if(str[i] === '1' && str[i+1] === '0') {
+            console.log(`i -> ${i} str[i] -> ${str[i]} str[i+1] -> ${str[i+1]}`);
+            binArr.push('0');
+            i += 1;
+            continue;
+        }
+
+        if(str[i] === '1' && str[i+1] === '1') {
+            binArr.push('1');
+            i += 1;
+            continue;
+        }
+
         if(str[i] === '0') {
             counter++;
             continue
@@ -62,11 +80,14 @@ function decode(str) {
     return decimal;
 }
 
-
 console.log(code('10111213'));
-/*
-console.log(decode(code('8')));
+console.log('11101111110110110111');
+console.log('--------------------------')
+console.log(decode(code('11101111110110110111')));
+console.log('10111213');
 
+
+/*
 console.log(code('47'));
 console.log(decode(code('47')));
 
