ca9fe34dc407997cb6017e642461fa549a7b08a7
tothricsaj <tothricsaj@gmail.com>
2020-01-20 16:42:02 +0100

    almost working :duck:

--- 6kyu/how-many-nines.js
+++ 6kyu/how-many-nines.js
@@ -6,7 +6,11 @@ function nines(n) {
     arr.shift();
     arr.push(n);
 
-    return arr;
+    return arr.reduce((acc, el) => {
+        let tmp = new String(el).toString();
+        console.log((tmp.match(/9/g) || []).length);
+        return (tmp.match(/9/g) || []).length;
+    }, 0);
 }
 
 console.log(nines(10));
