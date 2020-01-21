c30ae7daaca1b803ca77e7ec25e24f1fe5121d37
tothricsaj <tothricsaj@gmail.com>
2020-01-21 16:42:05 +0100

    bigint problem

--- 6kyu/how-many-nines.js
+++ 6kyu/how-many-nines.js
@@ -4,10 +4,11 @@ function nines(n) {
     arr.shift();
     arr.push(n);
 
-    return arr.reduce((acc, el) => {
-        return new String(el).toString().includes('9') ? acc + 1 : acc + 0;
-    }, 0);
-}
+    return BigInt(
+        arr.reduce((acc, el) => {
+            return new String(el).toString().includes('9') ? acc + 1 : acc + 0;
+        }, 0)
+    )}
 
 console.log(nines(3950));
-console.log(nines(90));
+console.log(nines(9));
