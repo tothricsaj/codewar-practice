5fb30869dc8a69a63b8205fcc4c4ad0b9ffd69c9
tothricsaj <tothricsaj@gmail.com>
2020-01-22 13:49:06 +0100

    code is working but no efficient enough :angry:

--- 6kyu/how-many-nines.js
+++ 6kyu/how-many-nines.js
@@ -1,5 +1,5 @@
 function nines(n) {
-    let arr = new Array(n).fill(0).map((el, i) => i);
+    let arr = new Array(Number(n)).fill(0).map((el, i) => i);
 
     arr.shift();
     arr.push(n);
@@ -8,7 +8,12 @@ function nines(n) {
         arr.reduce((acc, el) => {
             return new String(el).toString().includes('9') ? acc + 1 : acc + 0;
         }, 0)
-    )}
+    )
+}
 
-console.log(nines(3950));
-console.log(nines(9));
+console.log(nines(3950n));
+console.log(nines(9n));
+console.log(nines(1000n));
+
+
+console.log(21n);
