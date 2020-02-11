eac91548a3243cfbe5055742f080cae2602b574e
tothricsaj <tothricsaj@gmail.com>
2020-02-11 16:05:26 +0100

    other solution

--- 6kyu/group-anagrams.js
+++ 6kyu/group-anagrams.js
@@ -1,43 +1,37 @@
+// function groupAnagrams(words){
+//     let result = [];
+
+//     for(let i=0; i<words.length; i++) {
+
+//         let tmp = words.slice(i, words.length);
+//         let currentWord = words[i].split('').sort().toString();
+//         let anagArr = [];
+
+//         for(let n=0; n<tmp.length; n++) {
+
+//             let comparedWord = tmp[n].match(/[a-zA-z]/g).sort().toString();
+//             if(comparedWord === currentWord) {
+//                 anagArr.push(tmp[n]);
+//                 words.splice(words.indexOf(tmp[n]), 1)
+//             }
+
+//         }
+
+//         result.push(anagArr);
+//         i--;
+//     }
+
+//     return result;
+// }
+//
+
 function groupAnagrams(words){
-    let result = [];
-
-    for(let i=0; i<words.length; i++) {
-
-        let tmp = words.slice(i, words.length);
-        let currentWord = words[i].split('').sort().toString();
-        let anagArr = [];
-
-        console.log(`${words} - ${words[i]} - ${i}`);
-
-        // anagArr.push(words[i]);
-
-        for(let n=0; n<tmp.length; n++) {
-
-            let comparedWord = tmp[n].match(/[a-zA-z]/g).sort().toString();
-            // console.log(`currentWord - ${currentWord}; comparedWord - ${comparedWord}`)
-
-            // console.table({
-            //     n: n,
-            //     // words: words,
-            //     currentWord: currentWord,
-            //     comparedWord: comparedWord
-            // });
-
-            if(comparedWord === currentWord) {
-                anagArr.push(tmp[n]);
-                // console.log(words.splice(words.indexOf(tmp[n]), 1));
-                // console.warn('BAANG!!!!!');
-                words.splice(words.indexOf(tmp[n]), 1)
-                console.log(`This is words inner loop: ${words}\n`)
-            }
-
-        }
-
-        result.push(anagArr);
-        i--;
-    }
-
-    return result;
+  var groups = {};
+  words.forEach(function(w) {
+    var s = w.split('').sort().join('');
+    groups[s] = (groups[s] || []).concat([w]);
+  });
+  return Object.keys(groups).map(function(k) {return groups[k];});
 }
 
-console.log(groupAnagrams(["tar", "star", "tsar", "rat" ]));
+console.log(groupAnagrams(["tar", "star", "rat" ]));
