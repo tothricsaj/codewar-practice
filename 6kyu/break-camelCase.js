bd55180c12e318ff8ae7b5d77f9c42c359b88471
tothricsaj <tothricsaj@gmail.com>
2020-02-13 15:57:26 +0100

    maybe a solution

--- 6kyu/break-camelCase.js
+++ 6kyu/break-camelCase.js
@@ -1,5 +1,15 @@
 function solution(string) {
-    
+    let arr = string.split('')
+
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] === arr[i].toUpperCase()) {
+
+            arr.splice(i, 0, ' ');
+            i++;
+        }
+    }
+
+    return arr.join('');
 }
 
 
