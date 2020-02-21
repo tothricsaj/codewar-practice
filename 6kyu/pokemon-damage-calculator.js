6c673b20bd8899fbcce0b57adcc2f8b16517cb1b
tothricsaj <tothricsaj@gmail.com>
2020-02-21 16:00:20 +0100

    notNOTnotNOT

--- 6kyu/pokemon-damage-calculator.js
+++ 6kyu/pokemon-damage-calculator.js
@@ -29,6 +29,18 @@ function calculateDamage(yourType, opponentType, attack, defense){
         }
     } else if (yourType === 'grass' && opponentType === 'electric') {
         effectiveness = 1;
+    } else if (yourType === 'grass') {
+        switch(opponentType) {
+            case 'water':
+                effectiveness = 2;
+                break;
+            case 'fire':
+                effectiveness = 0.5;
+                break;
+            default:
+                effectiveness = 1;
+        }
+
     } else {
         effectiveness = 1;
     }
@@ -36,6 +48,10 @@ function calculateDamage(yourType, opponentType, attack, defense){
     return 50 * (attack / defense) * effectiveness
 }
 
-console.log(calculateDamage("fire", "water", 100, 100));
+// console.log(calculateDamage("fire", "water", 100, 100));
 console.log(calculateDamage("grass", "water", 100, 100))
-console.log(calculateDamage("electric", "fire", 100, 100))
+// console.log(calculateDamage("electric", "fire", 100, 100))
+// console.log(calculateDamage("grass", "electric", 57, 19))
+console.log(calculateDamage("grass", "water", 40, 40))
+console.log(calculateDamage("grass", "fire", 35, 5))
+// console.log(calculateDamage("fire", "electric", 10, 2))
