e71e46f70215df233c4b013383795f907789ef7b
tothricsaj <tothricsaj@gmail.com>
2020-02-25 07:02:50 +0100

    remIndex problem

--- 6kyu/a-rule-of-divisibility-by-13.js
+++ 6kyu/a-rule-of-divisibility-by-13.js
@@ -1,21 +1,29 @@
 function thirt(n) {
     let rem = [1, 10, 9, 12, 3, 4]
+    let remIdex = -1;
     let stationary
 
     if(n>=0) {
 
         let numArr = new String(n).split('').reverse();
 
-        return numArr.reduce((acc, val, i) => {
+        return numArr.reduce((acc, val) => {
             let digit = new Number(val).valueOf();
 
-            console.log(`i-> ${i} ----- digit -> ${digit}`)
-            console.log(`acc -> ${acc}`)
-            console.log(`rem[i] -> ${rem[i]}`)
+            remIdex++
 
-            if(i+1 >= rem.length) i = 0
+            console.table({
+                remIdex: remIdex,
+                currDigit: digit,
+                accumulator: acc,
+                remVal: rem[remIdex]
+            })
 
-            return rem[i]*digit + acc
+            if(remIdex >= rem.length) {
+                remIdex = 0
+            } 
+
+            return rem[remIdex]*digit + acc
 
         }, 0);
 
@@ -24,6 +32,6 @@ function thirt(n) {
     }
 }
 
-console.log(thirt(8529));
-// console.log(thirt(85299258))
+// console.log(thirt(8529));
+console.log(thirt(85299258))
 // console.log(thirt(5634))
