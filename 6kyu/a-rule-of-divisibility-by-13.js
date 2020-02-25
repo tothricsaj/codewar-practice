a1be2503cad722268cf7606e92253e816cc02816
tothricsaj <tothricsaj@gmail.com>
2020-02-25 06:29:44 +0100

    prepared the number

---
+++ 6kyu/a-rule-of-divisibility-by-13.js
@@ -0,0 +1,17 @@
+function thirt(n) {
+    let rem = [1, 10, 9, 12, 3, 4]
+    let stationary
+
+    if(n>=0) {
+
+        let numArr = new String(n).split('').reverse();
+
+        return numArr
+
+        return stationary
+    }
+}
+
+console.log(thirt(8529));
+console.log(thirt(85299258))
+console.log(thirt(5634))
