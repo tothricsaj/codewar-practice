2e71a4800534060ea9d050b5db4f946870af0453
tothricsaj <tothricsaj@gmail.com>
2020-03-02 16:01:29 +0100

    basic array

--- 6kyu/financing-plan-on-planet-XY140Z-n.js
+++ 6kyu/financing-plan-on-planet-XY140Z-n.js
@@ -2,6 +2,10 @@ function finance(n) {
     let days = []
     for(let i=0; i<=n; i++) days.push(new Array(n + 1).fill(0))
 
+    days.forEach((row, index) => {
+        row[index] = index * 2;
+    });
+
     return days
 }
 
