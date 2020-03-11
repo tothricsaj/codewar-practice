80ba5a71a9aac94869b4839f1edb09bd6fdd128b
tothricsaj <tothricsaj@gmail.com>
2020-03-11 10:48:14 +0100

    I can not see something

--- 6kyu/financing-plan-on-planet-XY140Z-n.js
+++ 6kyu/financing-plan-on-planet-XY140Z-n.js
@@ -1,6 +1,6 @@
 function finance(n) {
     let days = []
-    let addVal = 2
+    let addVal = 0
     let returnArr = () => {
         let arr = []
         for(let i=0; i<n; i++) {
@@ -11,13 +11,19 @@ function finance(n) {
 
     for(let i=0; i<n; i++) {
         days.push(returnArr().map((val, index) => {
-            return val + addVal
+            console.log(index)
+            if (index !== 0) return val + addVal
+            else return val
         }).fill(0, 0, i))
 
         addVal++
     }
 
-    return days
+    return days.reduce((acc, val, i) => {
+        return acc + val.reduce((accIn, valIn, iIn) => {
+            return accIn + valIn
+        }, 0)
+    }, 0)
 }
 
 console.log(finance(6))
