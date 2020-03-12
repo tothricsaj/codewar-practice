ce813cd82de4f4acdbc5dd38087b0bacc186c38b
tothricsaj <tothricsaj@gmail.com>
2020-03-12 11:29:29 +0100

    I don't have a clue what I am doing :confused:

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -1,10 +1,39 @@
 function send(text) {
-    let binaryCode = (text.charCodeAt() >>> 0).toString(2).split('')
-    return binaryCode.reduce((acc, el, i) => {
-        console.log(el)
-        if(el === '1') return array.slice()
-        else return el
-    }, [])
+    let binaryCode = (text.charCodeAt() >>> 0).toString(2)
+    let res = ''
+    let isOne = true
+
+    for(let i=0; i<binaryCode.length; i++) {
+        let buff = ''
+
+        if(binaryCode[i] === '1') isOne = true
+        else isOne = false
+
+        if(isOne) {
+            buff += '0 '
+
+            for(let ind = i; ind<binaryCode.length; ind++) {
+                if(binaryCode[ind] !== '1') break;
+                buff += '0'
+            }
+
+            buff += ' '
+        } else {
+            buff += ' 00 '
+
+            for(let ind = i; ind<binaryCode.length; ind++) {
+                if(binaryCode[ind] !== '1') break;
+                buff += '0'
+            }
+
+            buff += ' '
+
+        }
+
+        res += buff
+    }
+
+    return res
 }
 
 function receive(text) {
