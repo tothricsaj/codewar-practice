fa288f6319de0c168350e16643fd25ed24e85d0a
tothricsaj <tothricsaj@gmail.com>
2020-03-12 10:52:37 +0100

    wrong way?????

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -1,7 +1,14 @@
 function send(text) {
-  return
+    let binaryCode = (text.charCodeAt() >>> 0).toString(2).split('')
+    return binaryCode.reduce((acc, el, i) => {
+        console.log(el)
+        if(el === '1') return array.slice()
+        else return el
+    }, [])
 }
 
 function receive(text) {
   return
 }
+
+console.log(send('C'))
