944b9ca57ec9a937b8a312541a440e01e0d49f8b
tothricsaj <tothricsaj@gmail.com>
2020-03-13 10:07:24 +0100

    reference is added to check the next element

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -1,18 +1,18 @@
 function send(text) {
     let binaryCode = (text.charCodeAt()).toString(2)
     let res = ''
+    let referenc = binaryCode[0]
 
     console.log(`binaryCode -> ${binaryCode}`)
 
     if(binaryCode[0] === '1') res+='0 '
     else if(binaryCode[0] === '0') res+='00 '
 
-
     for(let i=1; i<binaryCode.length; i++) {
         let buff = ''
 
-        if(binaryCode[i+1] === '1') buff += ' 0 '
-        else if(binaryCode[i+1] === '0') buff += ' 00 '
+        if(binaryCode[i+1] === '1' && referenc === '1') buff += ' 0 '
+        else if(binaryCode[i+1] === '0' && referenc === '0') buff += ' 00 '
 
         buff += '0'
 
