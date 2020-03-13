91381c8894caa4628c7738270de37a2a9f47f3db
tothricsaj <tothricsaj@gmail.com>
2020-03-13 10:21:36 +0100

    reference checking is fixed

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -4,17 +4,28 @@ function send(text) {
     let referenc = binaryCode[0]
 
     console.log(`binaryCode -> ${binaryCode}`)
+    console.log('------------------------------------------')
 
-    if(binaryCode[0] === '1') res+='0 '
-    else if(binaryCode[0] === '0') res+='00 '
+    if(binaryCode[0] === '1') res+='0 0'
+    else if(binaryCode[0] === '0') res+='00 0'
 
     for(let i=1; i<binaryCode.length; i++) {
         let buff = ''
 
-        if(binaryCode[i+1] === '1' && referenc === '1') buff += ' 0 '
-        else if(binaryCode[i+1] === '0' && referenc === '0') buff += ' 00 '
+        if(binaryCode[i+1] === '1' && referenc !== '1') buff += ' 0 '
+        else if(binaryCode[i+1] === '0' && referenc !== '0') buff += ' 00 '
+
+        if(binaryCode[i+1] !== referenc) referenc = binaryCode[i+1]
 
         buff += '0'
+
+        console.table({
+            index: i,
+            currChar: binaryCode[i],
+            nextChar: binaryCode[i+1],
+            reference: referenc,
+            buff: buff
+        })
 
         res += buff
     }
