782a306349b010f8972f84ededf6f517c13e3cbf
tothricsaj <tothricsaj@gmail.com>
2020-03-18 09:27:29 +0100

    a recursive solution :smirk:

--- 6kyu/persistent-bugger.js
+++ 6kyu/persistent-bugger.js
@@ -1,7 +1,20 @@
 function persistence(num) {
     let digits = (num + '').split('')
-    return digits
+    
+    if(digits.length === 1) {
+        return 0
+    } else {
+        let mult = digits[0]
+        for(let i=1; i<digits.length; i++) {
+            mult *= digits[i]
+        }
+
+        return 1 + persistence(mult)
+    }
 }
 
 
 console.log(persistence(39))
+console.log(persistence(999))
+console.log(persistence(4))
+
