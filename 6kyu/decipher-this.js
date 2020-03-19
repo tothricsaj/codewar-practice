f946ac75ae96df31f58a7c4385da6f87c9fda122
tothricsaj <tothricsaj@gmail.com>
2020-03-19 12:15:08 +0100

    first character is deciphered

--- 6kyu/decipher-this.js
+++ 6kyu/decipher-this.js
@@ -1,5 +1,24 @@
 function decipherThis(str) {
-    return str
-}; 
+    
+    let arr = str.split(' ')
+
+    arr = arr.map((el, i) => {
+        let reg = /\d+/g
+        let firstChar = String.fromCharCode(el.match(reg))
+        let firstCharLength = el.match(reg)[0].length
+
+        console.table({
+            firstChar: firstChar,
+            firstCharLength: firstCharLength,
+            code: el.match(reg)[0]
+        })
+
+        return firstChar + el.slice(firstCharLength)
+
+        
+    })
+
+    return arr 
+}
 
 console.log(decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o'))
