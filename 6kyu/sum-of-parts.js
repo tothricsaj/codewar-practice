129b6340743120d9b4c4b255ed77697db8570306
tothricsaj <tothricsaj@gmail.com>
2020-03-20 18:18:07 +0100

    test cases are ok but timeout

--- 6kyu/sum-of-parts.js
+++ 6kyu/sum-of-parts.js
@@ -1,13 +1,18 @@
 function partsSums(ls) {
+    let res
 
     if(ls.length === 0) return [0]
 
-    return ls.map((el, i) => {
+    res = ls.map((el, i) => {
         let arr = ls.slice(i, ls.length)
         return arr.reduce((acc, num)=> {
            return acc+num
         }, 0)
     })
+
+    res.push(0)
+
+    return res
 }
 
 console.log(partsSums([0, 1, 3, 6, 10]))
