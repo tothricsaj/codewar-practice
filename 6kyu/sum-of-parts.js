8c1df8fd17f34e0e9db7997f385c7e3a1be5a42b
tothricsaj <tothricsaj@gmail.com>
2020-03-20 18:35:05 +0100

    recursive solution

--- 6kyu/sum-of-parts.js
+++ 6kyu/sum-of-parts.js
@@ -1,18 +1,44 @@
+// Iterrative solution (timeout but it works)
+//
+// function partsSums(ls) {
+//     let res
+
+//     if(ls.length === 0) return [0]
+
+//     res = ls.map((el, i) => {
+//         let arr = ls.slice(i, ls.length)
+//         return arr.reduce((acc, num)=> {
+//            return acc+num
+//         }, 0)
+//     })
+
+//     res.push(0)
+
+//     return res
+// }
+//
+
+let result = []
+
+function countElements(ls) {
+    return ls.reduce((a, b) => a+b)
+}
+
 function partsSums(ls) {
-    let res
 
-    if(ls.length === 0) return [0]
+    if(ls.length === 0) {
+        result.push(0)
+        return result
+    }
 
-    res = ls.map((el, i) => {
-        let arr = ls.slice(i, ls.length)
-        return arr.reduce((acc, num)=> {
-           return acc+num
-        }, 0)
-    })
 
-    res.push(0)
+    result.push(countElements(ls))
 
-    return res
+    ls.shift()
+
+    partsSums(ls)
+
+    return result
 }
 
 console.log(partsSums([0, 1, 3, 6, 10]))
