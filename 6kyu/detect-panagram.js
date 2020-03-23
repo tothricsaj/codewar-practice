c048a032c7e527ef0075bee0c350c61f5a5acf22
tothricsaj <tothricsaj@gmail.com>
2020-03-23 14:55:08 +0100

    solution ???? maybe :fire:

--- 6kyu/detect-panagram.js
+++ 6kyu/detect-panagram.js
@@ -1,5 +1,20 @@
 function isPangram(string){
-  //...
+
+    let count = 0
+    string = string.toLowerCase()
+
+    if(string.length <= 26) return false
+
+    string.split('').forEach((ch, i) => {
+        let charCode = string.charCodeAt(i)
+        if(
+            charCode >= 97 || charCode <= 122
+        ) {
+            count++
+        }
+    })
+
+    return count < 27 ? false : true
 }
 
 var string = "The quick brown fox jumps over the lazy dog."
