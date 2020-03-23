45d777b7870f568b7f82ca90eebbf4e6d66b04f0
tothricsaj <tothricsaj@gmail.com>
2020-03-23 16:04:28 +0100

    elegant solution

--- 6kyu/detect-panagram.js
+++ 6kyu/detect-panagram.js
@@ -1,20 +1,7 @@
 function isPangram(string){
 
-    let count = 0
-    let strSet = new Set(string.toLowerCase().split(''))
-
-    if(string.length <= 26) return false
-
-    strSet.forEach((ch, i) => {
-        let charCode = string.charCodeAt(i)
-        if(
-            charCode >= 97 || charCode <= 122
-        ) {
-            count++
-        }
-    })
-
-    return count < 27 ? false : true
+    let strSet = new Set(string.replace(/[^a-zA-Z]/gi,'').toLowerCase())
+    return strSet.size === 26
 }
 
 var string = "The quick brown fox jumps over the lazy dog."
