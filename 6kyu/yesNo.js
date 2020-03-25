3e4686b37e99e207c0dec644fd6ca0d4e7a76ea4
tothricsaj <tothricsaj@gmail.com>
2020-03-25 20:10:44 +0100

    is it working :fire: ????

--- 6kyu/yesNo.js
+++ 6kyu/yesNo.js
@@ -1,5 +1,25 @@
 function yesNo(arr){
+    let res = []
+    let yes = true
 
+    while(arr.length) {
+        for(let i=0; i<arr.length; i++) {
+            if(yes) {
+                res.push(arr[i])
+                arr.splice(i, 1)
+                i--
+            }
+
+            yes = !yes
+        }
+
+        console.table({
+            arr: arr,
+            res: res
+        })
+    }
+
+    return res
 }
 
-console.log(yesNo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(yesNo([1, 2, 3, 4, 5]))
