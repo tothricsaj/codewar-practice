6447ac4ae5153de087d334819e521d0ef1132dd5
tothricsaj <tothricsaj@gmail.com>
2020-05-25 14:52:56 +0200

    retry the unary chalange

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -1,52 +1,14 @@
-function binaryToUnary(binaryCode) {
-    let res = ''
-    let swap = true
-
-    if(binaryCode.length < 7) {
-        let tmp = binaryCode.split('')
-
-        for(let i=0; i< 7-binaryCode.length; i++) {
-            tmp.unshift(0)
-        }
-
-        binaryCode = tmp.join('')
-    }
-
-    for(let i=0; i<binaryCode.length; i++) {
-
-        if(binaryCode[i] === '1' && swap) {
-            res += '0 '
-            swap = false
-        } else if(binaryCode[i] === '0' && swap) {
-            res += '00 '
-            swap = false
-        }
-
-        res += '0'
-
-        if(binaryCode[i+1] !== binaryCode[i] && binaryCode[i+1] !== undefined) {
-            swap = !swap
-            res += ' '
-        }
-    }
-    
-    return res
-}
-
-
 function send(text) {
-    let binaryCode = text.split('').map((el) => {
-        return (el.charCodeAt()).toString(2)
-    }).join('')
+    let binaryCode = text.split('').map((el) => (el.charCodeAt()).toString(2)).join('')
 
-    return binaryToUnary(binaryCode)
+    return binaryCode
 }
 
 function receive(text) {
   return
 }
 
-console.log(send('%') + '\n')
+console.log(send('C') + '\n')
 console.log('')
 // console.log('#########################\n')
 // console.log(send('aC '))
