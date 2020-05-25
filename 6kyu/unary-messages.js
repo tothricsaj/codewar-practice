495c6e5bc409ca8c227e89aae0552bcd9b5ee87b
tothricsaj <tothricsaj@gmail.com>
2020-05-25 16:03:47 +0200

    receive solution

--- 6kyu/unary-messages.js
+++ 6kyu/unary-messages.js
@@ -11,6 +11,8 @@ function send(text) {
         if(binaryCode[i] === '0' && binaryCode[i+1] === '1') modified.push(' 0 ')
     }
 
+    console.log(binaryCode)
+
     return modified.map((e) => {
         if(e === '1') return '0'
         return e
@@ -18,10 +20,31 @@ function send(text) {
 }
 
 function receive(text) {
-  return
+    let arr = text.split(''),
+        first = 0,
+        middle = 1,
+        end = 2
+    let decoded = []
+
+    arr.forEach((el, i) => {
+        if(arr[first] === '0' && arr[middle] === ' ' && arr[end] === '0') decoded.push(1)
+        if(arr[first] === '0' && arr[middle] === '0' && arr[end] === '0') decoded.push(0)
+
+        end++
+
+        if(arr[end] === ' ') {
+            first = end + 1
+            middle = end + 2
+            end = middle + 1
+        }
+    })
+
+    return String.fromCharCode(parseInt(decoded.join(''), 2).toString(10))
 }
 
-console.log(send("Chuck Norris' keyboard has 2 keys: 0 and white space." + '\n'))
-console.log('')
+// console.log(send("C"))
+// console.log(receive(send("C")))
+
+console.log(receive('0 0 00 0000 0 0000 00 0 0 0 00 000 0 000 00 0 0 0 00 0 0 000 00 000 0 0000 00 0 0 0 00 0 0 00 00 0 0 0 00 00000 0 0 00 00 0 000 00 0 0 00 00 0 0 0000000 00 00 0 0 00 0 0 000 00 00 0 0 00 0 0 00 00 0 0 0 00 00 0 0000 00 00 0 00 00 0 0 0 00 00 0 000 00 0 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 000 00 000 0 0 00 0 0 00 00 0 0 000000 00 0000 0 0000 00 00 0 0 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 00 00 0 0 0 00 000 0 00 00 0000 0 0000 00 00 0 00 00 0 0 0 00 000000 0 00 00 00 0 0 00 00 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 0000 00 00 0 00 00 0 0 000 00 0 0 0 00 00 0 0 00 000000 0 00 00 00000 0 0 00 00000 0 00 00 0000 0 000 00 0 0 000 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 000 00 0 0 00000 00 0 0 0 00 000 0 00 00 0 0 0 00 00 0 0000 00 0 0 0 00 00 0 00 00 00 0 0 00 0 0 0 00 0 0 0 00 00000 0 000 00 00 0 00000 00 0000 0 00 00 0000 0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0'))
 // console.log('#########################\n')
 // console.log(send('aC '))
